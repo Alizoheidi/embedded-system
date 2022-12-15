99e7ad289b558e77970cd5376757e8237eccfa1f
Soheil <soheilprogrammer0@gmail.com>
2022-12-15 19:20:10 +0330

    formula changed

--- computing.py
+++ computing.py
@@ -25,21 +25,20 @@ def computing_results(graph, task_vertex):
     :param task_vertex: a task node which should be done
     """
     for processor in graph.give_nondirected_edges(task_vertex):
-        # calculate cycle per instruction for each task
+        # get properties of each task
         instruction_count, frequency_rate, cpi_instruction = graph.vertices_functions[task_vertex]
-        cpi = frequency_rate * cpi_instruction
-        # get frequency rate and clock rate of each processor
-        p_frequency_rate, p_clock_rate, p_consume_power = graph.vertices_functions[processor]
-        # performance = clock rate/CPI
-        performance = round(p_clock_rate / cpi, 3)
-        # add data to the table
-        performance_table.loc[processor][task_vertex] = performance
-        # instruction_count = clock_rate/CPI
-        instruction_count = p_clock_rate / cpi
-        # execution time = (instruction_count*CPI)/frequency_rate
-        execution_time = round((instruction_count * cpi) / p_frequency_rate, 3)
+        # get frequency ,cpi and clock rate of each processor
+        cpi, p_frequency, p_consume_power = graph.vertices_functions[processor]
+        # cycle time = 1/frequency
+        cycle_time = 1/p_frequency
+        # execution time = instruction_count * CPI * cycle time
+        execution_time = round(instruction_count * cpi * cycle_time, 3)
         # add data to the table
         execution_time_table.loc[processor][task_vertex] = execution_time
+        # performance = 1/execution time
+        performance = 1/execution_time
+        # add data to the table
+        performance_table.loc[processor][task_vertex] = performance
         # energy consumption = consume power * operational time
         energy_consumption = round(p_consume_power * operational_time_table.loc[processor][task_vertex],2)
         # add data to the table
--- ressource_library.py
+++ ressource_library.py
@@ -13,21 +13,21 @@ def add_resource_library_nodes(graph):
     each function return clock rate , frequency , consume power
     """
     def fpga():
-        return resource_lib_dict[PROCESSORS_NAME[0]]['clock rate'], \
+        return resource_lib_dict[PROCESSORS_NAME[0]]['CPI'], \
                resource_lib_dict[PROCESSORS_NAME[0]]['frequency'], \
                resource_lib_dict[PROCESSORS_NAME[0]]['consume power']
 
     graph.add_vertex(PROCESSORS_NAME[0], fpga())
 
     def risc():
-        return resource_lib_dict[PROCESSORS_NAME[1]]['clock rate'], \
+        return resource_lib_dict[PROCESSORS_NAME[1]]['CPI'], \
                resource_lib_dict[PROCESSORS_NAME[1]]['frequency'], \
                resource_lib_dict[PROCESSORS_NAME[1]]['consume power']
 
     graph.add_vertex(PROCESSORS_NAME[1], risc())
 
     def cisc():
-        return resource_lib_dict[PROCESSORS_NAME[2]]['clock rate'], \
+        return resource_lib_dict[PROCESSORS_NAME[2]]['CPI'], \
                resource_lib_dict[PROCESSORS_NAME[2]]['frequency'], \
                resource_lib_dict[PROCESSORS_NAME[2]]['consume power']
 
